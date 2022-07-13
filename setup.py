239855332c3f73779d47baac14546ace42d9f085
Leandro Koiti Sato <leandro@leandrosato.com>
2022-07-13 10:49:22 -0300

    fix version conflict when using pip install

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.7",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*!=3.5.*",
+
     install_requires=[
         "collective.transmogrifier",
         "plone.app.textfield",
